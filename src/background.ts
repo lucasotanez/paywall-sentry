a6ab2204d768533aa791768f1bb7f56fdd3523e2
Lucas Otanez <lotanez@usc.edu>
2023-07-09 21:55:21 -0700

    feat: run script on page update

--- src/background.ts
+++ src/background.ts
@@ -1,10 +1,28 @@
+// TODO:
+// Make script run everytime the user makes a search (requires manifest.json file too):
+// https://stackoverflow.com/questions/15149322/executing-chrome-extension-onclick-instead-of-page-load
+
 function getDiv(id : string) : void {
 
   // ========= Initialize hashmap here for now ==============
   let blacklist = new Map<string, boolean>()
-  blacklist.set("www.chegg.com", false)
-  blacklist.set("homework.study.com", false)
-  blacklist.set("www.numerade.com", false)
+  // true = hard paywall (unimplemented as of now)
+  // false = soft paywall (unimplemented)
+
+  // Academic Help
+  blacklist.set("www.chegg.com", true)
+  blacklist.set("homework.study.com", true)
+  blacklist.set("www.numerade.com", true)
+  blacklist.set("www.coursehero.com", true)
+  blacklist.set("brainly.com", true)
+  // News
+  blacklist.set("www.wsj.com", true)
+  blacklist.set("www.thetimes.co.uk", true)
+  blacklist.set("www.thesundaytimes.co.uk", true)
+  blacklist.set("www.kyivpost.com", true)
+  blacklist.set("www.latimes.com", false)
+  blacklist.set("www.washingtonpost.com", false)
+  blacklist.set("www.washingtontimes.com", false)
   // ========================================================
 
   let targetBox = document.getElementById(id)
@@ -22,28 +40,50 @@ function getDiv(id : string) : void {
     let base : RegExpExecArray | string | null = null
     if (typeof fullUrl == 'string' ) base = regExp.exec(fullUrl);
     if (base != null) base = base[1];
-    if (typeof base == 'string') {
-      if (!blacklist.has(base)) {
-        links.splice(index, 1);
-      } 
+    if (typeof base != 'string' || !blacklist.has(base)) {
+      index -= 1;
+      continue    
     }
-    index -= 1;
-  }
-  index = 0
-  while (index < links.length) {
+    let hard = blacklist.get(base) ? true : false
     let display = links[index].getElementsByTagName("h3")[0];
-    display.innerHTML = "[" + String.fromCodePoint(0x2718) +"] " + display.innerHTML;
-    display.style.color = "red"
-    index += 1;
+
+    if (hard) {
+      display.innerHTML = "[" + String.fromCodePoint(0x2718) + "] " + display.innerHTML;
+      display.style.color = "#d11919"
+    } else {
+      display.innerHTML = "[" + String.fromCodePoint(0x2757) + "] " + display.innerHTML;
+      display.style.color = "#a6ab1f"
+    }     
+
+    index -= 1;
   }
 
   return  
 }
 
-chrome.action.onClicked.addListener(async (tab) => {
+const filter = {
+  urls: ["https://www.google.com/search"]
+}
+
+chrome.tabs.onUpdated.addListener(async (tabId, changeInfo, tab) => {
+  if (!tab.url) return;
+  if (tab.url.includes('https://www.google.com/search'))
   chrome.scripting.executeScript({
     target: {tabId: tab.id ? tab.id : -1},
     func: getDiv,
     args: ["search"]
   }).then();
 });
+
+//chrome.tabs.query({
+//  "active": true,
+//}, function (tabs : chrome.tabs.Tab[]) {
+//  console.log("running")
+//  for (let tab in tabs) {
+//    let url = tabs[tab].url
+//    let tabid = tabs[tab].id ? tabs[tab].id : -1
+//    if (!(tabid && url)) continue
+//      console.log("passed checks")
+//
+//  }
+//})
